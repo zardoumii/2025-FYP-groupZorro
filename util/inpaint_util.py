8f7e7879ade4dc90a9deea41107e8f15e6750e77
yoal <yoal@itu.dk>
2025-04-30 12:38:04 +0200

    Added whitehat to hair removal

--- util/inpaint_util.py
+++ util/inpaint_util.py
@@ -1,16 +1,25 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-def removeHair(img_rgb, img_gray, kernel_size=5, threshold=100):
+def removeHair(img_rgb, img_gray, kernel_size=13, black_thresh=100, white_thresh=10):
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
 
 
     blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
+    _, black_thresh_mask = cv2.threshold(blackhat, black_thresh, 255, cv2.THRESH_BINARY)
 
 
-    _, thresh = cv2.threshold(blackhat, threshold, 255, cv2.THRESH_BINARY)
+    whitehat = cv2.morphologyEx(img_gray, cv2.MORPH_TOPHAT, kernel)
+    _, white_thresh_mask = cv2.threshold(whitehat, white_thresh, 255, cv2.THRESH_BINARY)
 
 
-    img_out = cv2.inpaint(img_rgb, thresh, inpaintRadius=1, flags=cv2.INPAINT_TELEA)
+    combined_mask = cv2.bitwise_or(black_thresh_mask, white_thresh_mask)
 
-    return blackhat, thresh, img_out
+
+    img_out = cv2.inpaint(img_rgb, combined_mask, inpaintRadius=1, flags=cv2.INPAINT_TELEA)
+
+    return blackhat, whitehat, combined_mask, img_out
+
+
